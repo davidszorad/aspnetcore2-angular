600257e5e1596e4cb1db8f2ffd7d8e9da3a10803
David Szorad <davidszorad@Davids-MacBook-Pro.local>
2017-11-29 21:47:33 +0100

    submitting vehicle form

--- ClientApp/app/services/vehicle.service.ts
+++ ClientApp/app/services/vehicle.service.ts
@@ -20,4 +20,9 @@ export class VehicleService {
       .map(res => res.json());
   }
 
+  create(vehicle: any) {
+    return this.http.post('/api/vehicles', vehicle)
+      .map(res => res.json());
+  }
+
 }
