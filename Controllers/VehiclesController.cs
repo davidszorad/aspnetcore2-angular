d39cab38270275688d41069a5223ddda43ed9894
David Szorad <david.szorad@outlook.com>
2017-11-30 22:24:53 +0100

    Feature/toast notifications (#3)
    * submitting vehicle form
    
    * Client side validations
    
    * initial commit

--- Controllers/VehiclesController.cs
+++ Controllers/VehiclesController.cs
@@ -40,6 +40,8 @@ namespace veganew.Controllers
         [HttpPost]
         public async Task<IActionResult> CreateVehicle([FromBody] SaveVehicleResource vehicleResource)
         {
+            // throw new Exception();
+            
             if (!ModelState.IsValid)
             {
                 return BadRequest(ModelState);
