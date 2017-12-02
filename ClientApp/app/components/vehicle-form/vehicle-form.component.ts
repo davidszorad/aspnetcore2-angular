f533b0f7a11853e2125213b0a579b8a123220ad7
David Szorad <davidszorad@Davids-MacBook-Pro.local>
2017-12-02 13:07:35 +0100

    Sending parallel requests

--- ClientApp/app/components/vehicle-form/vehicle-form.component.ts
+++ ClientApp/app/components/vehicle-form/vehicle-form.component.ts
@@ -2,6 +2,8 @@ import { VehicleService } from './../../services/vehicle.service';
 import { Component, OnInit, NgModule } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ToastyService } from 'ng2-toasty';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/Observable/forkJoin';
 
 @Component({
   selector: 'app-vehicle-form',
@@ -26,10 +28,32 @@ export class VehicleFormComponent implements OnInit {
       route.params.subscribe(p => {
         this.vehicle.id = +p['id']; // plus sign converts p['id'] to number
       });
-
     }
 
   ngOnInit() {
+    // Order does not matter as it will go to server in parallel
+    var sources = [
+      this.vehicleService.getMakes(),
+      this.vehicleService.getFeatures(),
+    ];
+    if (this.vehicle.id) {
+      sources.push(this.vehicleService.getVehicle(this.vehicle.id));
+    }
+    
+    Observable.forkJoin(sources)
+      .subscribe(data => {
+        this.makes = data[0];
+        this.features = data[1];
+        if (this.vehicle.id) {
+          this.vehicle = data[2];
+        }
+      }, err => {
+        if (err.status == 404) {
+          this.router.navigate(['/home']);
+        }
+      });
+    
+    /*
     if (this.vehicle.id) {
       this.vehicleService.getVehicle(this.vehicle.id)
         .subscribe(v => {
@@ -40,12 +64,11 @@ export class VehicleFormComponent implements OnInit {
           }
         });
     }
-
     this.vehicleService.getMakes()
       .subscribe(makes => this.makes = makes);
-    
     this.vehicleService.getFeatures()
       .subscribe(features => this.features = features);
+    */
   }
 
   /*
