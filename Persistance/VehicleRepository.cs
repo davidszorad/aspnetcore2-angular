a1aacb477b40fdab3a7733510ba27b03c6a8b0a8
David Szorad <davidszorad@Davids-MacBook-Pro.local>
2017-12-08 20:44:34 +0100

    Pagination (vehicles)

--- Persistance/VehicleRepository.cs
+++ Persistance/VehicleRepository.cs
@@ -68,6 +68,8 @@ namespace veganew.Persistance
             // query = ApplyOrdering(queryObj, query, columnsMap);
             query = query.ApplyOrdering(queryObj, columnsMap);
 
+            query = query.Skip((queryObj.Page - 1) * queryObj.PageSize).Take(queryObj.PageSize);  // Paging is done in SQL server not in memory
+
             return await query.ToListAsync();            
         }
 
