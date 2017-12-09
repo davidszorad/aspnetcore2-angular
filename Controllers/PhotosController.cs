25efa951e4f4192022e98e87b60524b1dfd9bce7
David Szorad <davidszorad@Davids-MacBook-Pro.local>
2017-12-09 22:38:05 +0100

    MOD: using configuration

--- Controllers/PhotosController.cs
+++ Controllers/PhotosController.cs
@@ -6,6 +6,7 @@ using AutoMapper;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 using veganew.Controllers.Resources;
 using veganew.Core;
 using veganew.Core.Models;
@@ -17,19 +18,26 @@ namespace veganew.Controllers
     [Route("/api/vehicles/{vehicleId}/photos")]
     public class PhotosController : Controller
     {
-        private readonly int MAX_BYTES = 1 * 1024 * 1024; // 10 MB
-        private readonly string[] ACCEPTED_FILE_TYPES = new[] { ".jpeg", ".jpg", ".png" };
+        // private readonly int MAX_BYTES = 1 * 1024 * 1024; // 10 MB
+        // private readonly string[] ACCEPTED_FILE_TYPES = new[] { ".jpeg", ".jpg", ".png" };
         
         private readonly IHostingEnvironment host;
         private readonly IVehicleRepository repository;
         private readonly IUnitOfWork unitOfWork;
         private readonly IMapper mapper;
-        public PhotosController(IHostingEnvironment host, IVehicleRepository repository, IUnitOfWork unitOfWork, IMapper mapper)
+        private readonly PhotoSettings photoSettings;
+        public PhotosController(
+            IHostingEnvironment host, 
+            IVehicleRepository repository, 
+            IUnitOfWork unitOfWork, 
+            IMapper mapper,
+            IOptionsSnapshot<PhotoSettings> options)
         {
             this.mapper = mapper;
             this.unitOfWork = unitOfWork;
             this.repository = repository;
             this.host = host;
+            this.photoSettings = options.Value;
         }
 
         [HttpPost]
@@ -41,8 +49,8 @@ namespace veganew.Controllers
 
             if (file == null) return BadRequest("Null file");
             if (file.Length == 0) return BadRequest("Empty file");
-            if (file.Length > MAX_BYTES) return BadRequest("Maximum file size exceeded");
-            if (!ACCEPTED_FILE_TYPES.Any(s => s == Path.GetExtension(file.FileName))) return BadRequest("Invalid file type");
+            if (file.Length > photoSettings.MaxBytes) return BadRequest("Maximum file size exceeded");
+            if (!photoSettings.IsSupported(file.FileName)) return BadRequest("Invalid file type";
 
             var uploadsFolderPath = Path.Combine(host.WebRootPath, "uploads"); // wwwroot/uploads
             if (!Directory.Exists(uploadsFolderPath))
